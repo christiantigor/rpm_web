38f204914ff3a7f2e963016a68c1b74ba0c6a2a9
christiantigor <christian.tigor@rocketmail.com>
2014-09-30 11:57:56 +0700

    Correct web navigation and captcha

--- captcha.php
+++ captcha.php
@@ -1,4 +1,4 @@
-<?
+<?php
 session_start(); 
 $_SESSION["simpankata"]="";
 //if (!extension_loaded('gd')) dl('gd.so');
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 	//set base_url, misal: http://rpm.com/
-	$base_url = "http://localhost/rpm/";
+	$base_url = "http://m2m.tritronik.com/rpm/index.php?modul=";
 	
 	//set title website dan file website
 	require_once("functions.php");
